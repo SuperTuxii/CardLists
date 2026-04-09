a4b5ba1d5a0c648d22310e501b543f917b73d8c3
SuperTuxii <123881249+SuperTuxii@users.noreply.github.com>
2026-04-10 00:39:07 +0200

    Finish Import Popup Implementation for obsidian notes
    Finish Import Popup Implementation to make it
    possible to import obsidian notes via the popup.
    
    Signed-off-by: SuperTuxii <123881249+SuperTuxii@users.noreply.github.com>

--- src/ImportPopup.jsx
+++ src/ImportPopup.jsx
@@ -7,6 +7,7 @@ import {WebsocketContext} from "./WebsocketContext.jsx";
 function ImportPopup() {
     const socket = useContext(WebsocketContext);
     const [fileInfos, setFileInfos] = useState([]);
+    const [fileData, setFileData] = useState([]);
 
     async function getAPI(url){
         // return (await axiosToastIfError(axios.get("http://localhost:8080/api/get", { params: { url: url }}))).data;
@@ -26,69 +27,80 @@ function ImportPopup() {
             <div id="popup">
                 <h2>Import Anime</h2>
                 <div className={"vertical"}>
-                    <input type={"file"} multiple onChange={(e) => {
-                        console.log(e);
-                        setFileInfos([]);
-                        for (let file of e.target.files) {
-                            const reader = new FileReader();
-                            reader.onerror = () => setFileInfos([...fileInfos, `${file.name} reading failed`]);
-                            reader.onload = async () => {
-                                let result = reader.result;
-                                if (file.type === "text/markdown") {
-                                    const data = { name: file.name.slice(0, file.name.lastIndexOf(".")) };
-                                    result = result.slice(result.indexOf("---") + 4, result.indexOf("---", result.indexOf("---") + 3));
-                                    const regex = /(\w+): (.*)/g;
-                                    const dataKeys = [
-                                        "name",
-                                        "series",
-                                        "seriesPart",
-                                        "season",
-                                        "status",
-                                        "timestamp",
-                                        "ownStatus",
-                                        "filmId",
-                                        "anisearchUrl"
-                                    ]
-                                    if (!result.match(regex)) {
-                                        setFileInfos([...fileInfos, `${data.name} can't find properties`]);
-                                        console.warn(`${data.name} can't find properties`);
-                                        return;
-                                    }
-                                    for (let match of result.match(regex)) {
-                                        const key = match.replace(regex, "$1");
-                                        if (!dataKeys.includes(key))
-                                            continue;
-                                        let value = match.replace(regex, "$2");
-                                        if (value.startsWith('"'))
-                                            value = JSON.parse(value.toString());
-                                        if (key === "season")
-                                            value = Number(value).toString();
-                                        data[key] = value;
-                                    }
-                                    if (dataKeys.some((key) => !(key in data))) {
-                                        setFileInfos([...fileInfos, `${data.name} doesn't include all required keys`]);
-                                        console.warn(`${data.name} doesn't include all required keys`);
-                                        return;
-                                    }
-                                    const dbData = await getAPI(data.anisearchUrl);
-                                    if (dbData.fromDB) {
-                                        setFileInfos([...fileInfos, `${data.name} is already in DB`]);
-                                        console.warn(`${data.name} is already in DB`);
-                                        return;
-                                    }
-                                    if ("series" in dbData && dbData.series !== data.series) {
-                                        setFileInfos([...fileInfos, `${data.name} doesn't match proposed series name (${dbData.series})`]);
-                                        console.warn(`${data.name} doesn't match proposed series name (${dbData.series})`);
-                                        return;
-                                    }
-                                    //TODO: do adding of the anime
+                    <input type={"file"} multiple onChange={async (e) => {
+                        let newFileInfos = [];
+                        let newFileData = [];
+                        const filePromises = [...e.target.files].map((file) => {
+                            return new Promise((resolve, reject) => {
+                                const reader = new FileReader();
+                                reader.onload = () => resolve({ file: file, result: reader.result});
+                                reader.onerror = reject;
+                                reader.readAsText(file);
+                            });
+                        });
+
+                        for (const info of await Promise.all(filePromises)) {
+                            if (info.file.type === "text/markdown") {
+                                const data = {name: info.file.name.slice(0, info.file.name.lastIndexOf("."))};
+                                info.result = info.result.slice(info.result.indexOf("---") + 4, info.result.indexOf("---", info.result.indexOf("---") + 3));
+                                const regex = /(\w+): (.*)/g;
+                                const dataKeys = [
+                                    "name",
+                                    "series",
+                                    "seriesPart",
+                                    "season",
+                                    "status",
+                                    "timestamp",
+                                    "ownStatus",
+                                    "filmId",
+                                    "anisearchUrl"
+                                ]
+                                if (!info.result.match(regex)) {
+                                    newFileInfos.push(`"${data.name}" can't find properties`);
+                                    console.warn(`${data.name} can't find properties`);
+                                    continue;
                                 }
-                            };
-                            reader.readAsText(file);
+                                for (let match of info.result.match(regex)) {
+                                    const key = match.replace(regex, "$1");
+                                    if (!dataKeys.includes(key))
+                                        continue;
+                                    let value = match.replace(regex, "$2");
+                                    if (value.startsWith('"'))
+                                        value = JSON.parse(value.toString());
+                                    if (key === "season")
+                                        value = Number(value).toString();
+                                    data[key] = value;
+                                }
+                                if (dataKeys.some((key) => !(key in data))) {
+                                    newFileInfos.push(`"${data.name}" doesn't include all required keys`);
+                                    console.warn(`${data.name} doesn't include all required keys`);
+                                    continue;
+                                }
+                                const url = data.anisearchUrl;
+                                delete data.anisearchUrl;
+                                const dbData = await getAPI(url);
+                                if (dbData.fromDB) {
+                                    newFileInfos.push(`"${data.name}" is already in DB`);
+                                    console.warn(`${data.name} is already in DB`);
+                                    continue;
+                                }
+                                if ("series" in dbData && dbData.series !== data.series) {
+                                    newFileInfos.push(`"${data.name}" doesn't match proposed series name (${dbData.series})`)
+                                    console.warn(`${data.name} doesn't match proposed series name (${dbData.series})`);
+                                    continue;
+                                }
+                                newFileData.push({url: url, data: data});
+                            }
                         }
+                        setFileInfos(newFileInfos);
+                        setFileData(newFileData);
                     }} />
                     <div>{fileInfos.map((fileInfo, index) => <p key={index}>{fileInfo}</p>)}</div>
-                    <button>Import Anime</button>
+                    <button onClick={() => {
+                        for (let fileDatum of fileData) {
+                            addAPI(fileDatum.data, fileDatum.url);
+                        }
+                    }}>Import Anime</button>
                 </div>
             </div>
         </>
